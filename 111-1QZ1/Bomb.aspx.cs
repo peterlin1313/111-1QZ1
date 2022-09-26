0779442a23ae098c637a85189e7a631d206563ca
peterlin1313 <snakemanlin@gmail.com>
2022-09-26 13:08:45 +0800

    1213

---
+++ 111-1QZ1/Bomb.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ1
+{
+    public partial class Bomb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] ia_MIndex = new int[10] { 0, 7, 13, 28, 44, 62, 74, 87, 90 };
+            char[,] ia_Map = new char[10, 10];
+            for (int i_Row=0; i_Row< 10;> i_Row++) {
+                for (int i_Col = 0; i_Col < 10; i_Col++) {
+                    ia_Map[i_Row, i_Col] = '';
+                }
+            }
+
+
+            for (int i_Ct = 0; i_Ct <10; i_Ct++)
+            {
+                int i_Row = ia_MIndex[i_Ct] / 10;
+                int i_Col = ia_MIndex[i_Ct] % 10;
+                ia_Map[i_Row, i_Col] = '*';
+            }
+
+
+            for(int i_Row = 0; i_Row < 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    if (ia_Map[i_Row, i_Col] == ' ') { 
+                        Response.Write("&nsp;")
+                    }
+                    else
+                    {
+                        Response.Write(ia_Map[i_Row, i_Col]);
+                    }
+                }
+            }
+        }
+    }
+}
